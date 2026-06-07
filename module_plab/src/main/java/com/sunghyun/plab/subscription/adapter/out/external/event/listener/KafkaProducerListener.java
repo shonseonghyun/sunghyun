f1460655a9fcdf84354c5c3cac7f3e7611e56e81
손성현 <sunghyun7895@naver.com>
2026-06-08 01:50:06 +0900

    알림 발송 체계 카프카 전환 v1.6 - KafkaProducerListener 내 복구 가능/불가능 예외 후처리 분기 개발

--- module_plab/src/main/java/com/sunghyun/plab/subscription/adapter/out/external/event/listener/KafkaProducerListener.java
+++ module_plab/src/main/java/com/sunghyun/plab/subscription/adapter/out/external/event/listener/KafkaProducerListener.java
@@ -39,8 +39,23 @@ public class KafkaProducerListener implements ProducerListener<String, Notificat
         NotificationRequestedEvent event = producerRecord.value();
         final String outBoxNo = event.getOutBoxNo();
 
-        log.error("카프카 전송 실패! 아웃박스 ID=[{}]", outBoxNo, exception);
+        if (isNonRetryableException(exception)) {
+            log.error("[영구 실패] 개발자 긴급 확인 필요! 재시도 불가 에러 발생. ID=[{}]", outBoxNo, exception);
+            // 슬랙 알림 발송 로직 추가 가능 (slackWebhook.send(...))
 
-        outBoxCommandService.updateStatus(outBoxNo, false);
+//            outBoxCommandService.updateToPermanentFailed(outBoxNo); // 영구 실패 상태 마킹
+        }
+        // 💡 2. 회복 가능한 일시적 예외
+        else {
+            log.warn("[일시적 실패] 재시도 위해 상태값 변경. ID=[{}]", outBoxNo, exception);
+            outBoxCommandService.updateStatus(outBoxNo, false); // 일반 FAILED 상태 마킹 (스케줄러가 주워갈 타겟)
+        }
+    }
+
+    private boolean isNonRetryableException(Exception ex) {
+        return ex instanceof org.apache.kafka.common.errors.SerializationException ||
+                ex instanceof org.apache.kafka.common.errors.RecordTooLargeException ||
+                ex instanceof org.apache.kafka.common.errors.InvalidTopicException
+                ;
     }
 }
