a4ef2d6fb6e5eeb7b67a4da7a091d454c073f8ce
손성현 <sunghyun7895@naver.com>
2026-06-08 01:29:01 +0900

    알림 발송 체계 카프카 전환 v1.5 - KafkaProducerListener 도입 및 예외 처리 구조 단일화

--- module_plab/src/main/java/com/sunghyun/plab/config/KafkaProducerConfig.java
+++ module_plab/src/main/java/com/sunghyun/plab/config/KafkaProducerConfig.java
@@ -1,5 +1,6 @@
 package com.sunghyun.plab.config;
 
+import lombok.RequiredArgsConstructor;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
@@ -8,15 +9,19 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.support.ProducerListener;
 import org.springframework.kafka.support.serializer.JsonSerializer;
 
 import java.util.HashMap;
 import java.util.Map;
 
 @Configuration
+@RequiredArgsConstructor
 public class KafkaProducerConfig {
     @Value("${spring.kafka.bootstrap-server}")
     private String bootStrapServer;
+
+    private final ProducerListener producerListener;
 
     @Bean
     public ProducerFactory<String,Object> producerFactory(){
@@ -41,6 +46,8 @@ public class KafkaProducerConfig {
 
     @Bean
     public KafkaTemplate<String,Object> kafkaTemplate(ProducerFactory<String,Object> producerFactory){
-        return new KafkaTemplate<>(producerFactory);
+        KafkaTemplate<String,Object> kafkaTemplate = new KafkaTemplate<>(producerFactory);
+        kafkaTemplate.setProducerListener(producerListener);
+        return kafkaTemplate;
     }
 }
--- module_plab/src/main/java/com/sunghyun/plab/subscription/adapter/out/external/event/NotificationEventSpringEventListener.java
+++ module_plab/src/main/java/com/sunghyun/plab/subscription/adapter/out/external/event/NotificationEventSpringEventListener.java
@@ -67,19 +67,22 @@ public class NotificationEventSpringEventListener {
     public void sendEventV2(final NotificationRequestedEvent notificationRequestedEvent){
         log.info("AfterCommit 후 카프카 이벤트 발행v2 start. ID=[{}]", notificationRequestedEvent.getOutBoxNo());
 
-        CompletableFuture<SendResult<String,Object>> future =  kafkaTemplate.send(topic,notificationRequestedEvent);
+        kafkaTemplate.send(topic,notificationRequestedEvent);
+
+//        CompletableFuture<SendResult<String,Object>> future =  kafkaTemplate.send(topic,notificationRequestedEvent);
 
         // 비동기 콜백 등록
         // 단, 메타 데이터 조회 시 발생한 에러는 잡히지 않기에 catch 구문으로 잡거나 ProducerListener를 통해 후처리해야 한다.
-        future.whenComplete((result,ex)->{
-            if(ex==null){
-                // 성공
-                outBoxCommandService.updateStatus(notificationRequestedEvent.getOutBoxNo(), true);
-            } else{
-                log.error("카프카 이벤트 발행 중 예외가 발생하여 아웃박스를 FAILED로 변경합니다. [{}]", notificationRequestedEvent.getOutBoxNo(), ex);
-                outBoxCommandService.updateStatus(notificationRequestedEvent.getOutBoxNo(),false);
-            }
-        });
+//        future.whenComplete((result,ex)->{
+//            if(ex==null){
+//                // 성공
+//                outBoxCommandService.updateStatus(notificationRequestedEvent.getOutBoxNo(), true);
+//            } else{
+//                // 실패
+//                log.error("카프카 이벤트 발행 중 예외가 발생하여 아웃박스를 FAILED로 변경합니다. [{}]", notificationRequestedEvent.getOutBoxNo(), ex);
+//                outBoxCommandService.updateStatus(notificationRequestedEvent.getOutBoxNo(),false);
+//            }
+//        });
 
         log.info("아웃박스 테이블 상태 변경 최종 반영 완료");
     }
