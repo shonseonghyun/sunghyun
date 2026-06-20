a6a93bf70282e6e7fca78ceeba06b0b2b2bbe07b
손성현 <sunghyun7895@naver.com>
2026-06-20 14:11:47 +0900

    알림 발송 체계 카프카 전환 v1.9 - 메시지 소비 과정에서 예외 발생 시 DLT 방식 사용하지 않아 producer 설정값 제거

--- module_notification/src/main/java/com/sunghyun/notification/config/KafkaConsumerConfig.java
+++ module_notification/src/main/java/com/sunghyun/notification/config/KafkaConsumerConfig.java
@@ -2,18 +2,16 @@ package com.sunghyun.notification.config;
 
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
-import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
-import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
-import org.springframework.kafka.core.*;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 import org.springframework.kafka.listener.ContainerProperties;
 import org.springframework.kafka.listener.DefaultErrorHandler;
 import org.springframework.kafka.support.serializer.JsonDeserializer;
-import org.springframework.kafka.support.serializer.JsonSerializer;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -67,6 +65,9 @@ public class KafkaConsumerConfig {
         factory.setConsumerFactory(consumerFactory());
         factory.getContainerProperties().setAckMode(ContainerProperties.AckMode.MANUAL_IMMEDIATE);
         factory.setCommonErrorHandler(defaultErrorHandler);
+
+        factory.setConcurrency(3);
+
         return factory;
     }
 
@@ -92,20 +93,4 @@ public class KafkaConsumerConfig {
 //        return errorHandler;
 //    }
 
-
-    // DLT(Dead Letter Topic)에 발행할 메시지 템플릿
-    @Bean
-    public KafkaTemplate<String, Object> kafkaTemplate(ProducerFactory<String, Object> producerFactory) {
-        return new KafkaTemplate<>(producerFactory);
-    }
-
-    // DLT(Dead Letter Topic)에 메시지 발행시 사용할 Producer 설정 세팅
-    @Bean
-    public ProducerFactory<String, Object> producerFactory() {
-        Map<String, Object> config = new HashMap<>();
-        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
-        config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
-        return new DefaultKafkaProducerFactory<>(config);
-    }
 }
