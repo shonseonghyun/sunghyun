b123980d6e625f09f665188983d53a03d2e01e65
shson <sunghyun7895@naver.com>
2026-06-29 18:24:16 +0900

    plab 도메인 엔드포인트에 대한 SecurityMatcherChain 권한 설정 등록 및 MemberIdPendingEventListener 주석 이유 기재

--- module_member/src/main/java/com/sunghyun/member/adpater/out/eventListener/MemberIdPendingEventListener.java
+++ module_member/src/main/java/com/sunghyun/member/adpater/out/eventListener/MemberIdPendingEventListener.java
@@ -5,8 +5,6 @@ import com.sunghyun.member.application.port.MemberIdPendingRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Propagation;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.transaction.event.TransactionPhase;
 import org.springframework.transaction.event.TransactionalEventListener;
 
@@ -16,7 +14,7 @@ public class MemberIdPendingEventListener {
     private final MemberIdPendingRepository memberIdPendingRepository;
 
     @Async
-    @Transactional(propagation = Propagation.REQUIRES_NEW)
+//    @Transactional(propagation = Propagation.REQUIRES_NEW) //RDB 쓰기 작업이 없는 순수 레디스 로직이므로 무의미한 DB 커넥션 점유 방지
     @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
     public void handle(final MemberRegisteredEvent event) {
         final String id = event.getId();
---
+++ module_plab/src/main/java/com/sunghyun/plab/config/PlabSecurityConfig.java
@@ -0,0 +1,27 @@
+package com.sunghyun.plab.config;
+
+import com.sunghyun.config.authorize.Role;
+import com.sunghyun.config.authorize.SecurityMatcherChain;
+import com.sunghyun.config.authorize.SecurityRequestMatcher;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class PlabSecurityConfig {
+
+    @Bean
+    public SecurityMatcherChain securityMatcherChain(){
+        SecurityMatcherChain securityMatcherChain = new SecurityMatcherChain();
+
+        securityMatcherChain.addAll(
+                SecurityRequestMatcher.patterns(
+                        "/plab/subscription/**"
+                ).hasRole(Role.USER),
+                SecurityRequestMatcher.patterns(
+                        "/plab/match/**"
+                ).hasRole(Role.ADMIN)
+        );
+
+        return securityMatcherChain;
+    }
+}
