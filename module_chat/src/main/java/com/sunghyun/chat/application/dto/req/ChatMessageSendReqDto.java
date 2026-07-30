2e942c977d4b857a76ded5a90525d18bb74d6368
shson <sunghyun7895@naver.com>
2026-07-30 21:45:46 +0900

    채팅 서비스 개발 v1.8 - 불필요한 필드 제거

--- module_chat/src/main/java/com/sunghyun/chat/application/dto/req/ChatMessageSendReqDto.java
+++ module_chat/src/main/java/com/sunghyun/chat/application/dto/req/ChatMessageSendReqDto.java
@@ -2,16 +2,10 @@ package com.sunghyun.chat.application.dto.req;
 
 import com.sunghyun.chat.domain.message.enums.ChatMessageType;
 import lombok.Getter;
-import lombok.ToString;
 
 @Getter
-@ToString
 public class ChatMessageSendReqDto {
-    private Long chatMessageNo;
-
     private String senderName;
-    private Long senderMemberNo;
-
     private ChatMessageType messageType;
     private String content;
 }
